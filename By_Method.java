4cbf19eb69fc9da80122c41586d78a396cc37d1d
mouakong12 <101863295+mouakong12@users.noreply.github.com>
2022-04-22 21:29:52 +0700

    Add files via upload

---
+++ By_Method.java
@@ -0,0 +1,5 @@
+public class By_Method {
+    String id = "204N";
+    String name = "MouaKong";
+    String lastname = "THAO";
+}
---
+++ By_Reference_Variable.java
@@ -0,0 +1,6 @@
+public class By_Reference_Variable {
+    void display()
+    {
+        System.out.println("Java Method");
+    }
+}
